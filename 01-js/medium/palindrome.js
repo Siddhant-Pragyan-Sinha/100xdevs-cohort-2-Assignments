9c2e978823d7f92ad6c82c227e198908f39374b5
umabharathi.s <umabharathi.s@zohocorp.com>
2024-05-10 22:51:26 +0530

    assignment 1

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,17 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let left = 0;
+  let right = str.length-1;
+  while (left < right){
+    if (str[left] != str[right]){
+      return false;
+    } else {
+      left ++;
+      right --;
+    }
+  }
   return true;
 }
 
