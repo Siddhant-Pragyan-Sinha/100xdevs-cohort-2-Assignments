5532f7b1c593b6f3cdd3042ea6e3f10ccc0d2834
khandayharoon <khandayharoon@gmail.com>
2024-10-31 15:40:41 +0530

    daysame

--- week-pg-2/dist/update-data.js
+++ week-pg-2/dist/update-data.js
@@ -10,28 +10,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const _1 = require(".");
-function insertData(email) {
+function updateDateForAUser(userId) {
     return __awaiter(this, void 0, void 0, function* () {
         const client = yield (0, _1.getClient)();
-        try {
-            const insertDataQuery = `
-    INSERT INTO Insta_App (first_name, last_name, email, password)
-    VALUES ($1, $2, $3, $4)
-    `;
-            const res = yield (client === null || client === void 0 ? void 0 : client.query(insertDataQuery, [
-                "Haroon",
-                "Khanday",
-                email,
-                "password123",
-            ]));
-            console.log("success", res === null || res === void 0 ? void 0 : res.rows[1]);
-        }
-        catch (e) {
-            console.log("Something went wrong while inserting the data into the instausers table", e);
-        }
-        finally {
-            client === null || client === void 0 ? void 0 : client.end();
-        }
+        const updateQuery = `
+   UPDATE Insta_App
+   SET first_name = $1 ,last_name = $2
+   Where id = $3
+   `;
+        const res = yield (client === null || client === void 0 ? void 0 : client.query(updateQuery, ["newNameChanged", "newNamechanged23", userId]));
+        console.log(res === null || res === void 0 ? void 0 : res.rows);
     });
 }
-insertData("hk3@example.com");
+updateDateForAUser(9);
