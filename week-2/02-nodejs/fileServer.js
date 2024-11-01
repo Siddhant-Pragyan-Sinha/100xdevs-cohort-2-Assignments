c7943c0a9818752c0121586330497ab7213822ec
Hadii <135013774+hadirasool46@users.noreply.github.com>
2024-11-01 21:21:42 +0530

    solved one question of express

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -5,6 +5,7 @@
   1. GET /files - Returns a list of files present in `./files/` directory
     Response: 200 OK with an array of file names in JSON format.
     Example: GET http://localhost:3000/files
+
   2. GET /file/:filename - Returns content of given file by name
      Description: Use the filename from the request path parameter to read the file from `./files/` directory
      Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
@@ -16,6 +17,39 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+
+app.get("/files", (req, res) => {
+  const fileAddress= path.join(__dirname, "./files")
+  fs.readdir(fileAddress, (err, data) => {
+    if (err) {
+      console.log(error)
+      res.status(500).json({ "error": "error reading data" })
+    } else {
+      res.status(200).json(data);
+
+    }
+  }
+  )
+})
+
+
+app.get("/file/:filename", (req,res)=>{
+  
+    const filename = req.params.filename
+    const filePath = path.join(__dirname, "./files", filename)
+    fs.readFile(filePath, utf-8, (err,data) =>{
+    if(err){
+      console.log(err)
+      res.status(404).json({"message": "not found"})
+    } else{
+      res.status(200).json(data)
+    }
+  })
+})
+
+app.use((req,res) => {
+  res.status(404).send("route not found ")
+})
 
 
 module.exports = app;
