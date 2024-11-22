7f70bc71305bf5f6641403314eb1f27dfa854e1c
iamnikhilranjan <nikhil21907@gmail.com>
2024-11-22 11:45:37 +0530

    03 mongo assignment

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -5,14 +5,36 @@ const userMiddleware = require("../middleware/user");
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.create({
+        username, 
+        password
+    })
+    res.json({
+       message: "User created successfully"
+    })
+
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async(req, res) => {
     // Implement listing all courses logic
+    const response = await Course.find({
+        // isPublished: true
+    });
+    
+    res.json({
+        courses: response
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    Purchases.create({
+        userId,
+        courseId
+    })
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
