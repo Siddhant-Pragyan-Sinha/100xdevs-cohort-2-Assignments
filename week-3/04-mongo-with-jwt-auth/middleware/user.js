2d89947486e4886443d982d98294569da5ca327d
umabharathi.s <umabharathi.s@zohocorp.com>
2024-12-17 18:23:31 +0530

    week 3 and 4 assignments

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,20 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../index");
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token = req.header.authorization;
+    const words = token.split(" ");
+    const jwttoken = words[1];
+    const decodedvalue = jwt.verify(jwttoken,secret);
+    if (decodedvalue.username) {
+        req.username = decodedvalue.username;
+        next();
+    } else {
+        res.status(403).json({
+            msg : "You are not authenticated"
+        })
+    }
 }
 
 module.exports = userMiddleware;
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,74 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const {Admin,User, Course} = require("../db")
+const {jwt} = require("jsonwebtoken");
+const secret = require("../index");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username : username,
+        password : password
+    })
+    res.json({
+        message : 'Admin created successfully'
+    })
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = Admin.find({
+        username : username,
+        password : password
+    })
+    if (user){
+        const token = jwt.sign({
+            username
+        },secret)
+        res.json({
+            token : token
+        })
+    } else {
+        res.status(403).json({
+            message : "Invalid input"
+        })
+    }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async(req, res) => {
     // Implement course creation logic
+   const title = req.body.title;
+   const description = req.body.description;
+   const imageLink = req.body.imageLink;
+   const price = req.body.price;
+
+   const newCourse = await Course.create({
+  title,
+  description,
+    imageLink,
+    price
+   })
+   res.json({
+    message : "Your course created successfully",
+    courseId  : newCourse._id
+   })
+   
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async(req, res) => {
     // Implement fetching all courses logic
+    const response = await Course.find({})
+    res.json({
+        courses : response
+    })
+
 });
 
 module.exports = router;
